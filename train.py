8028840e6e9fcca5207fdfa262cbd449cdf92582
raven4752 <raven4752@foxmail.com>
2018-04-19 12:56:40 +0800

    limit gpu memory. fix  a bug

--- train.py
+++ train.py
@@ -14,11 +14,13 @@ num_val_batches = 256
 
 data_generator = ProgramGenerator(batch_size=training_batch_size, program_length=1, num_len=1)
 # data_generator = AdditionGenerator(batch_size=training_batch_size)
-
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
 with tf.device('/gpu:0'):
-    with tf.Session() as session:
+    with tf.Session(config=config) as session:
 
         print("Building model")
+
         model = Seq2SeqModel(session=session,
                              hidden_units=hidden_units,
                              num_layers=num_layers,
@@ -27,7 +29,6 @@ with tf.device('/gpu:0'):
                              num_input_symbols=num_symbols,
                              num_output_symbols=num_symbols,
                              batch_size=training_batch_size,
-                             is_training=True,
                              scope='model')
 
         model.init_variables()
